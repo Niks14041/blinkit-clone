7e0b57d5f8a1868e408af63264271f9767ea9bdf
sumanmunde123 <93462136+sumanmunde123@users.noreply.github.com>
2022-04-03 17:03:56 +0530

    cart and payment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Cart } from './componants/Cart';
+import { Payment } from './componants/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      <Cart/>
+      <Payment/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/Payment.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import './Payment.css';
+
+function Payment() {
+    return (
+        <div className="pay">
+            <div className="pay-main">
+                <div className="pay-left">
+                    <div className="pay-left-head">Payment</div>
+                    <div className="pay-total-div">
+                        <div className="color">
+                            <span>Total Amount</span>
+                            <span>₹446</span>
+                        </div>
+                        <div className="pay-total-line"></div>
+                        <div>
+                            <span>Amount Payable</span>
+                            <span>₹446</span>
+                        </div>
+                    </div>
+                    <div className="pay-card-box">
+                        <form>
+                            <label htmlFor="card-number">Card Number</label>
+                            <input type="number" value="" className="card-number" placeholder="Card Number" maxLength={16} />
+                            <label htmlFor="valid-through" className="valid">Valid Through</label>
+                            <input type="num" className="card-date" maxLength={2} placeholder="MM" />
+                            <input type="num" className="card-date" placeholder="YY" maxLength={2} />
+                            <label htmlFor="cvv" className="cvv">CVV</label>
+                            <input type="num" className="card-cvv" maxLength={3} />
+                            <input type="submit" className="card-pay-btn" value={"PayNow"}/>
+                        </form>
+                    </div>
+                </div>
+                <div className="pay-right">
+                    <div className="pay-right-head">
+                        <span>My Cart</span>
+                        <span>2 items</span>
+                    </div>
+                    <div className="pay-right-item">
+                        <div className="pay-right-pro">
+                            <div className="pay-item-img">
+                                <img width={"100%"} src="https://cdn.grofers.com/app/images/products/sliding_image/11020a.jpg" alt="" />
+                            </div>
+                            <div className="pay-item-details">
+                                <div className="item-det-head">Cadbury Dairy Milk Home</div>
+                                <div className="item-det-price">₹145</div>
+                            </div>
+                        </div>
+                        <div className="pay-right-pro">
+                            <div className="pay-item-img">
+                                <img width={"100%"} src="https://cdn.grofers.com/app/images/products/sliding_image/11020a.jpg" alt="" />
+                            </div>
+                            <div className="pay-item-details">
+                                <div className="item-det-head">Cadbury Dairy Milk Home</div>
+                                <div className="item-det-price">₹145</div>
+                            </div>
+                        </div>
+                        <div className="pay-right-pro">
+                            <div className="pay-item-img">
+                                <img width={"100%"} src="https://cdn.grofers.com/app/images/products/sliding_image/11020a.jpg" alt="" />
+                            </div>
+                            <div className="pay-item-details">
+                                <div className="item-det-head">Cadbury Dairy Milk Home</div>
+                                <div className="item-det-price">₹145</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { Payment };
